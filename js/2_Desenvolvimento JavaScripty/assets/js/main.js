1270bf60c4ac6607704945a6571d06f1e3a0f67d
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-09-13 17:42:02 -0300

    Update main.js

--- js/2_Desenvolvimento JavaScripty/assets/js/main.js
+++ js/2_Desenvolvimento JavaScripty/assets/js/main.js
@@ -11,7 +11,9 @@ function loadPokemonItens(offset, limit) {
       .map((pokemon) => {
         return `
        
-  <li data-pokemon='${JSON.stringify(pokemon)}' onclick="openModal(this)"class="pokemon ${pokemon.type} buttonDetails" >
+  <li data-pokemon='${JSON.stringify(
+    pokemon
+  )}' onclick="openModal(this)"class="pokemon ${pokemon.type} buttonDetails" >
           <span class="number">#${pokemon.number}</span>
           <span class="name">${pokemon.name}</span>
   
@@ -51,7 +53,7 @@ loadMoreButton.addEventListener("click", () => {
 function openModal(element) {
   const pokemonJSON = element.getAttribute("data-pokemon");
   const pokemon = JSON.parse(pokemonJSON);
-  console.log("openModal called")
+  console.log("openModal called");
   const modalContent = `
   <div class="modalDetais ${pokemon.type}">
   <div class="modalTitle">
@@ -81,7 +83,6 @@ function openModal(element) {
   // Show the modal
   modalInser.style.display = "flex";
 }
-
 
 function closeModal() {
   modalInser.style.display = "none";
