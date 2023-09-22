b40b4deea438b364c5b12898290c686c853ce753
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-09-22 20:35:09 -0300

    add desacoplação

--- Angular/4_blog/angular-blog/src/app/components/big-card/big-card.component.ts
+++ Angular/4_blog/angular-blog/src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
+  @Input()
+  photoCover: string = ''; //https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgGxVS04wVZ7Wrerbk-oBGs2I1Ymz2u3t-XoQZ7XDIBUSFNGmfy1YQbqwNetxydDF_3eW5BlQz7RlqJ7RacwzVKg_r4mgql9H9exLsJzhxnvVuQ2Jg1q42WK1YpHBuQITQk6ds3zEqpM-iV2R1DdsXW2v76kMsiUUAr3wksovlAXzUyXdmIxP9tdpdA7lo/s2048/jujutsu-kaisen-fas-lamentam-a-morte-de-um-amado-personagem-2.jpg
+  @Input()
+  cardTitle: string = ''; //Como conseguir ter meia entrada no cinema
+  @Input()
+  cardDescription: string = ''; //  Segundo o grande mestre Saturgojo do anime e manga Jujutsu Kaisen, você so
+  // precisa ter uma amizade verdadeira com o Sukuna...
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
