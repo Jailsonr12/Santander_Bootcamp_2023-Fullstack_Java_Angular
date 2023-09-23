795a16a75cd80b3029054e6911c58db1aedecbf6
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-09-23 13:28:59 -0300

    finalização da video aula começo do desafio

--- Angular/4_blog/angular-blog/src/app/components/big-card/big-card.component.ts
+++ Angular/4_blog/angular-blog/src/app/components/big-card/big-card.component.ts
@@ -7,13 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
   @Input()
-  photoCover: string = ''; //https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgGxVS04wVZ7Wrerbk-oBGs2I1Ymz2u3t-XoQZ7XDIBUSFNGmfy1YQbqwNetxydDF_3eW5BlQz7RlqJ7RacwzVKg_r4mgql9H9exLsJzhxnvVuQ2Jg1q42WK1YpHBuQITQk6ds3zEqpM-iV2R1DdsXW2v76kMsiUUAr3wksovlAXzUyXdmIxP9tdpdA7lo/s2048/jujutsu-kaisen-fas-lamentam-a-morte-de-um-amado-personagem-2.jpg
+  photoCover: string = '';
   @Input()
-  cardTitle: string = ''; //Como conseguir ter meia entrada no cinema
+  cardTitle: string = '';
   @Input()
-  cardDescription: string = ''; //  Segundo o grande mestre Saturgojo do anime e manga Jujutsu Kaisen, você so
-  // precisa ter uma amizade verdadeira com o Sukuna...
-
+  cardDescription: string = '';
+  @Input()
+  id: string = '0';
   constructor() {}
 
   ngOnInit(): void {}
