182fe0a65f58d419c8193f54326f849599f48642
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-09-25 23:43:57 -0300

    utilizando service

---
+++ Angular/6_serve/src/app/models/pokemonData.ts
@@ -0,0 +1,11 @@
+export type PokemonData = {
+  id: number;
+  name: string;
+  sprites: { front_default: string };
+  types: [
+    {
+      slot: number;
+      type: { name: string; url: string };
+    }
+  ];
+};
